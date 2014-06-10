50ef9730a0a363a47f103b26cde22fe3fb7bf6da
Thomas Levine <_@thomaslevine.com>
2014-06-09 23:24:56 -0400

    -

--- glass.hs
+++ glass.hs
@@ -4,12 +4,10 @@ type Cell a = (ColumnId, a)
 type Row = [Cell String] -- ugly
 type Table = [Row]
 
-type Glaze = Table -> Table
-
-cast :: ColumnId -> ColumnId -> Glaze
+cast :: ColumnId -> ColumnId -> Table -> Table
 cast dependent independent = undefined
 
-melt :: ColumnId -> ColumnId -> [ColumnId] -> [ColumnId] -> Glaze
+melt :: ColumnId -> ColumnId -> [ColumnId] -> [ColumnId] -> Row -> Table
 melt variableName valueName idVars measureVars = undefined
 
 meltSimple = melt "variable" "value"
