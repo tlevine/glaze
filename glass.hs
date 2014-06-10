b1fbedd423b5d2bf0dd7faf605b3500d67f03b46
Thomas Levine <_@thomaslevine.com>
2014-06-09 23:15:27 -0400

    -

--- glass.hs
+++ glass.hs
@@ -1,21 +1,13 @@
+data ColumnId = String
 
-type Column = String | Int
+type Cell a = (ColumnId, a)
+type Row => [Column]
+type Table => [Row]
 
-cast :: Column -> Column -> [Record] -> Record
+type Glaze = [ColumnId] -> Table -> Table
+type Glaze = [ColumnId] -> Table -> Table
 
-dataset1 = [ (("col1",1),("col2",2),("category","elephant"), ("value",4)),
-            (("col1",1),("col2",2),("category","whale"), ("value",8)),
-            (("col1",3),("col2",2),("category","whale"), ("value",8)) ]
-cast "category" "elephant" dataset1 ==  [ (("col1", 1), ("col2", 2), ("elephant",4), ("whale", 8)),
-                                          (("col1", 3), ("col2", 2), ("elephant",Nil), ("whale", 8)) ]
-dataset2 = ^^that
+cast :: ColumnId -> ColumnId -> Glaze
+cast dependent independent = undefined
 
-meltDependent :: Column -> [Record] -> Record
-meltDependent [0] dataset2 ==  [ (("col1", 1), ("key", "col2"), ("value", 2)),
-                                  ("col1", 1), ("key", "elephant"), ("value", 4)),
-                                  ("col1", 1), ("key", "whale"), ("value", 8)),
-                                  ("col1", 3), ("key", "col2"), ("value", 2)),
-                                  ("col1", 3), ("key", "elephant"), ("value", Nil)),
-                                  ("col1", 3), ("key", "whale"), ("value", 8)) ]
 
-meltDependent [0] dataset2 == meltDependent [2,3,4] dataset2
